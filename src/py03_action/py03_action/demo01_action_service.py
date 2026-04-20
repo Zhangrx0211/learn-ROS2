f588f235154abc13640ef3388041d525b1152a14
zhangrx <zhangruixiong909@gmail.com>
2026-04-20 19:09:47 +0800

    4

---
+++ src/py03_action/py03_action/demo01_action_service.py
@@ -0,0 +1,74 @@
+"""  
+    需求:编写动作服务端实习,可以提取客户端请求提交的整型数据,并累加从1到该数据之间的所有整数以求和，
+       每累加一次都计算当前运算进度并连续反馈回客户端，最后，在将求和结果返回给客户端。
+    步骤：
+        1.导包；
+        2.初始化 ROS2 客户端；
+        3.定义节点类；
+            3-1.创建动作服务端；
+            3-2.生成连续反馈；(回调函数)
+            3-3.生成最终响应。(回调函数)
+        4.调用spin函数,并传入节点对象；
+        5.释放资源。
+"""
+
+# 1.导包；
+import time
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+
+from base_interfaces_demo.action import Progress
+
+# 3.定义节点类；
+class ProgressActionServer(Node):
+    def __init__(self):
+        super().__init__('progress_action_server')
+
+        # ***3-1.创建动作服务端；                  接口文件   话题名称    回调函数 
+        self._action_server = ActionServer(self,Progress,'get_sum',self.callback)
+        self.get_logger().info('动作服务已经启动！')
+
+    #回调函数            目标值
+    def callback(self, goal_handle):
+        self.get_logger().info('开始执行任务....')
+
+
+     
+
+        num = goal_handle.request.num #获取目标值
+        sum = 0
+        for i in range(1, num + 1):
+            sum += i
+
+            # 生成连续反馈；
+            feedback_msg = Progress.Feedback()
+            feedback_msg.progress = float(i) / num #项目进度赋值
+            goal_handle.publish_feedback(feedback_msg) #发布完整的消息            
+            self.get_logger().info('连续反馈: %.2f' % feedback_msg.progress)
+ 
+            time.sleep(1) #休眠1秒
+ 
+        # 3-3.生成最终响应。
+        goal_handle.succeed()
+        result = Progress.Result()
+        result.sum = sum
+        self.get_logger().info('计算结果：%d' % result.sum)
+
+        return result
+
+
+def main(args=None):
+
+    # 2.初始化 ROS2 客户端；
+    rclpy.init(args=args)
+
+    # 4.调用spin函数，并传入节点对象；
+    Progress_action_server = ProgressActionServer()
+    rclpy.spin(Progress_action_server)
+
+    # 5.释放资源。
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
