45e56d2ee5fc374e45124f6031d21a33fb0d7c67
zhangrx <zhangruixiong909@gmail.com>
2026-04-20 15:18:10 +0800

    2

---
+++ src/py02_service/py02_service/demo01_service.py
@@ -0,0 +1,48 @@
+"""  
+    需求：编写服务端，接收客户端发送请求，提取其中两个整型数据，相加后将结果响应回客户端。
+    步骤：
+        1.导包；
+        2.初始化 ROS2 客户端；
+        3.定义节点类；
+            3-1.创建服务端；
+            3-2.处理请求数据并响应结果。
+        4.调用spin函数,并传入节点对象；
+        5.释放资源。
+
+"""
+
+# 1.导包；
+import rclpy
+from rclpy.node import Node
+from base_interfaces_demo.srv import Add
+
+# 3.定义节点类；
+class MinimalService(Node):
+
+    def __init__(self):
+        super().__init__('minimal_service_py')
+
+        self.get_logger().info("服务端启动！")
+        # ***3-1.创建服务端；           服务接口类型 话题名称  回调函数
+        self.srv = self.create_service(Add, 'add_ints', self.add_callback)
+
+
+    # ***3-2.处理请求数据并响应结果。
+    def add_callback(self, request, response):
+        response.sum = request.num1 + request.num2
+        self.get_logger().info('请求数据:(%d,%d),响应结果:%d' % (request.num1, request.num2, response.sum))
+        return response 
+
+
+def main():
+    # 2.初始化 ROS2 客户端；
+    rclpy.init()
+    # 4.调用spin函数，并传入节点对象；
+    minimal_service = MinimalService()
+    rclpy.spin(minimal_service)
+    # 5.释放资源。
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/py02_service/py02_service/demo02_client.py
@@ -0,0 +1,69 @@
+"""  
+    需求：编写客户端，发送两个整型变量作为请求数据，并处理响应结果。
+    步骤：
+        1.导包；
+        2.初始化 ROS2 客户端；
+        3.定义节点类；
+            3-1.创建客户端；
+            3-2.等待服务连接；
+            3-3.组织请求数据并发送；
+        4.创建对象调用其功能，处理响应结果；
+        5.释放资源。
+
+"""
+# 1.导包；
+import sys
+import rclpy
+from rclpy.node import Node
+from base_interfaces_demo.srv import Add
+import sys
+
+# 3.定义节点类；
+class MinimalClient(Node):
+    
+
+    def __init__(self):
+        super().__init__('minimal_client_py')
+
+        # ***3-1.创建客户端；
+        self.cli = self.create_client(Add, 'add_ints')
+
+        # ***3-2.等待服务连接；如果1秒内连接不上，循环下面的这个语句。
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('服务连接中，请稍候...')
+        
+
+    # 3-3.组织请求数据并发送；
+    def send_request(self):
+        #创建Add接口的请求
+        req = Add.Request()
+        req.num1 = int(sys.argv[1])
+        req.num2 = int(sys.argv[2])
+        #接受数据
+        self.future = self.cli.call_async(self.req)
+
+
+def main():
+    # 2.初始化 ROS2 客户端；
+    rclpy.init()
+    client = MinimalClient()
+
+    #发送请求
+    client.send_request()
+
+    # 负责接受服务端响应回来的结果。
+    rclpy.spin_until_future_complete(client,client.future)
+    
+    try:
+        response = client.future.result()
+    except Exception as e:
+        client.get_logger().info('服务请求失败： %r' % (e,))
+    else:
+        client.get_logger().info('响应结果： %d + %d = %d' %(response.sum))
+
+    # 5.释放资源。
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
