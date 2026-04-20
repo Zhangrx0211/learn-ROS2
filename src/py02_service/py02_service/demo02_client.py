9550e7aeae0bc181c81655484c516daa3f25bad8
zhangrx <zhangruixiong909@gmail.com>
2026-04-20 15:38:00 +0800

    5

--- src/py02_service/py02_service/demo02_client.py
+++ src/py02_service/py02_service/demo02_client.py
@@ -33,14 +33,31 @@ class MinimalClient(Node):
             self.get_logger().info('服务连接中，请稍候...')
         
 
-    # 3-3.组织请求数据并发送；
-    def send_request(self):
-        #创建Add接口的请求
-        req = Add.Request()
-        req.num1 = int(sys.argv[1])
-        req.num2 = int(sys.argv[2])
-        #接受数据
+    # # 3-3.组织请求数据并发送；
+    # def send_request(self):
+    #     #创建Add接口的请求
+    #     req = Add.Request()
+    #     req.num1 = int(sys.argv[1])
+    #     req.num2 = int(sys.argv[2])
+    #     #发送请求
+    #     self.future = self.cli.call_async(self.req)
+
+        # 创建请求
+        self.req = Add.Request()
+        self.req.num1 = 3
+        self.req.num2 = 5
+
+       # 发送请求（异步）
         self.future = self.cli.call_async(self.req)
+
+        # 设置回调，服务端处理完——执行callback
+        self.future.add_done_callback(self.callback)
+        
+    #***接受结果
+    def callback(self, future):
+        result = future.result() #获取返回值
+        self.get_logger().info(f'结果: {result.sum}')
+ 
 
 
 def main():
@@ -48,20 +65,22 @@ def main():
     rclpy.init()
     client = MinimalClient()
 
-    #发送请求
-    client.send_request()
+    rclpy.spin(client)
 
-    # 负责接受服务端响应回来的结果。
-    rclpy.spin_until_future_complete(client,client.future)
+    # #发送请求
+    # client.send_request()
+
+    # # 负责接受服务端响应回来的结果。
+    # rclpy.spin_until_future_complete(client,client.future)
     
-    try:
-        response = client.future.result()
-    except Exception as e:
-        client.get_logger().info('服务请求失败： %r' % (e,))
-    else:
-        client.get_logger().info('响应结果： %d + %d = %d' %( response.sum))
+    # try:
+    #     response = client.future.result()
+    # except Exception as e:
+    #     client.get_logger().info('服务请求失败： %r' % (e,))
+    # else:
+    #     client.get_logger().info('响应结果： %d + %d = %d' %( response.sum))
 
-    # 5.释放资源。
+    # # 5.释放资源。
     rclpy.shutdown()
 
 
